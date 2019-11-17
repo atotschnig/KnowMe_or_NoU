d9e205881b3f2d6a8275f5cec6afb42532231203
Agnes Totschnig <agnes@totschnig.org>
2019-11-17 00:22:43 -0500

    incorporating arduino values into quiz game

--- quiz.py
+++ quiz.py
@@ -1,7 +1,10 @@
+#Title: BostonHacks2019
+#Contributers: Agnes Totschnig, Saumyaa Verma
+
 import json
 import random
-#Title: BostonHacks2019
-#Contributers: Agnes Totschnig, Saumyaa Verma 
+import serial
+import time
 
 
 ################################################################################
@@ -24,14 +27,46 @@ def shortlist(questions, topic_wanted):
             shortlisted.append(q)
     return shortlisted
 
+
+def getLatestStatus():
+    status = ser.readline()
+    while ser.inWaiting() > 0:
+        status = ser.readline()
+    string_n = status.decode()
+    string = string_n.rstrip()
+    return string
+
+def check_answer(buttons):
+    #print('checks', buttons)
+    for index in range(4):
+        if buttons[index] == '1':
+            #print('answered', index + 1)
+            return True, index+1
+    return False, 0
+
+def read_arduino():
+    #print('reading')
+    correct, guess = 0, 0
+    answered1, answered2 = False, False
+    while not(answered1 and answered2):
+        buttons = getLatestStatus()
+        if not answered1:
+            answered1, correct = check_answer(buttons[0:4])
+        if not answered2:
+            answered2, guess = check_answer(buttons[4:8])
+    return correct, guess
+
 ################################################################################
 
 
 filename = input("Enter file name: ") + '.json'
 
+# import questions from json file
 with open(filename) as json_file:
     questions = json.load(json_file)
 
+
+# sort all questions by topics and ask for topic choice from user
 topics = list_topics(questions)
 print("The available topics are:" , topics)
 valid = False
@@ -42,6 +77,11 @@ while not valid:
         print("OOPS, couldn't find this topic! Re-enter topic name")
 shortlisted = shortlist(questions, topic_wanted)
 
+
+# set up arduino input
+ser = serial.Serial('/dev/cu.usbmodem14201',9600)
+
+
 num_q = len(questions)
 num_q_game = 3
 score = 0
@@ -51,12 +91,11 @@ for index_q in range(num_q_game):
     print(q['question'])
     for index_a in range(4):
         print(str(index_a + 1) + '. ' + q['answer' + str(index_a + 1)])
-    correct = int(input("Enter your correct answer: "))
-    guess = int(input("Enter your guess: "))
+    correct, guess = read_arduino()
     if guess == correct:
         score += 1
         print("Correct answer! :D")
     else:
         print("Wrong answer! :(")
 print("Your score is:",  score)
- 
+
